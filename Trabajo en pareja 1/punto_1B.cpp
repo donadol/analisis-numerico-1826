a995f2de76740d5d61df8c14f4001f019ef6b829
donadol <ldonado11@gmail.com>
2018-08-02 13:53:31 -0500

    Update punto_1B.cpp

--- Trabajo en pareja 1/punto_1B.cpp
+++ Trabajo en pareja 1/punto_1B.cpp
@@ -15,11 +15,37 @@ double eval(double p[], int n, double x){
     s=s+p[i]*pow(x, n-i-1);
   return s;
 }
-
+int derivate(double p[], int n){
+   for(int i=0; i<n; i++){
+    p[i]=p[i]*(n-i-1);
+  }
+  n--;
+  return n;
+}
+void printFcn(double p[], int n){
+  for(int i=0; i<n; i++){
+    if(p[i]!=0){
+      cout<<p[i];
+      if((n-i-1)!=0)
+        cout<<"x^"<<n-i-1;
+    if(i<n-1)
+      cout<<" + ";
+    }
+  }
+}
 int main() {
-  double p[]={7,6,-6,3,4};
+  double p[]={7,6,-6,0,3,-4};
   int n=sizeof(p)/sizeof(double);
   double x=3;
+  cout<<"La funcion a resolver es: "<<endl;
+  printFcn(p,n);
+  cout<<endl<<endl;
   cout<<"Resultado por metodo de Horner: "<<horner(p,n,x)<<endl;
-  cout<<"Resultado: "<<eval(p,n,x);
+  cout<<"Resultado evaluacion normal: "<<eval(p,n,x)<<endl<<endl;
+  cout<<"Derivando se obtiene: "<<endl;
+  n=derivate(p,n);
+  printFcn(p,n);
+  cout<<endl<<endl;
+  cout<<"Resultado por metodo de Horner: "<<horner(p,n,x)<<endl;
+  cout<<"Resultado evaluacion normal: "<<eval(p,n,x)<<endl;
 }
