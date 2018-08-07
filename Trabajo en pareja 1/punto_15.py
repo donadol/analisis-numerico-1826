4eca35149b673bc421e295d207007814e5b6e864
donadol <ldonado11@gmail.com>
2018-08-07 18:36:46 -0500

    Update punto_15.py

--- Trabajo en pareja 1/punto_15.py
+++ Trabajo en pareja 1/punto_15.py
@@ -6,6 +6,9 @@ def g(x):
 def error(x):
   return abs((g(x)-x)/g(x))*100
 
+def dg(x):
+  return math.exp(x)/5
+
 print('Ecuación a resolver: g(x)=(1+e^x)/5 en el intervalo [0,1]')
 print('Ingrese x para aproximación inicial:')
 x=float(input('x='))
@@ -13,5 +16,7 @@ print('     x   g(x) Error%')
 for i in range(5):
   x=round(x,4)
   e=round(error(x),4)
-  print(repr(x).ljust(6,'0'),format(g(x),'.4g'), repr(e).ljust(6,'0'))
+  if abs(dg(x))<1:
+    z='Cumple criterio de convergencia del punto fijo'
+  print(repr(x).ljust(6,'0'),format(g(x),'.4g'), repr(e).ljust(6,'0'), z)
   x=g(x)
