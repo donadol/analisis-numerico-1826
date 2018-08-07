a0fc54467f9d8f1a707ebfde0a834e1a5e67a332
donadol <ldonado11@gmail.com>
2018-08-07 16:01:15 -0500

    Update punto_1B.cpp

--- Trabajo en pareja 1/punto_1B.cpp
+++ Trabajo en pareja 1/punto_1B.cpp
@@ -3,22 +3,33 @@
 
 using namespace std;
 
-double horner(double p[], int n, double x){
+double horner(double p[], int n, double x, int &o){
   double y=p[0];
-  for(int i=1; i<n; i++)
+  for(int i=1; i<n; i++){
+    if(x<y)
+      o+=x;
+    else
+      o+=y;
+    o++;
     y=x*y+p[i];
+  }
   return y;
 }
-double eval(double p[], int n, double x){
+double eval(double p[], int n, double x, int &o){
   double s=0;
-  for(int i=0; i<n; i++)
+  for(int i=0; i<n; i++){
+    o++;
+    if(pow(x, n-i-1)<p[i])
+      o+=pow(x, n-i-1);
+    else
+     o+=p[i];
     s=s+p[i]*pow(x, n-i-1);
+  }
   return s;
 }
 int derivate(double p[], int n){
-   for(int i=0; i<n; i++){
+  for(int i=0; i<n; i++)
     p[i]=p[i]*(n-i-1);
-  }
   n--;
   return n;
 }
@@ -37,15 +48,19 @@ int main() {
   double p[]={7,6,-6,0,3,-4};
   int n=sizeof(p)/sizeof(double);
   double x=3;
+  int o=0;
   cout<<"La funcion a resolver es: "<<endl;
   printFcn(p,n);
   cout<<endl<<endl;
-  cout<<"Resultado por metodo de Horner: "<<horner(p,n,x)<<endl;
-  cout<<"Resultado evaluacion normal: "<<eval(p,n,x)<<endl<<endl;
+  cout<<"Resultado por metodo de Horner: "<<horner(p,n,x,o)<<" con "<<o<<" operaciones"<<endl;
+  o=0;
+  cout<<"Resultado evaluacion normal: "<<eval(p,n,x,o)<<" con "<<o<<" operaciones"<<endl<<endl;
   cout<<"Derivando se obtiene: "<<endl;
   n=derivate(p,n);
   printFcn(p,n);
   cout<<endl<<endl;
-  cout<<"Resultado por metodo de Horner: "<<horner(p,n,x)<<endl;
-  cout<<"Resultado evaluacion normal: "<<eval(p,n,x)<<endl;
+  o=0;
+  cout<<"Resultado por metodo de Horner: "<<horner(p,n,x,o)<<" con "  <<o<<" operaciones"<<endl;
+  o=0;
+  cout<<"Resultado evaluacion normal: "<<eval(p,n,x,o)<<" con "  <<o<<" operaciones"<<endl;
 }
