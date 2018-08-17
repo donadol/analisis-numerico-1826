fe9025172d1fb5f2f7e10e9071ced847df83261e
JhonnyParraB <41595878+JhonnyParraB@users.noreply.github.com>
2018-08-17 08:44:32 -0500

    Create algo.r

---
+++ algo.r
@@ -0,0 +1,19 @@
+evaluarFuncion<-function(f, x){
+  return(eval(f))
+}
+
+
+ProgramacionLinealBinaria<-function(z, n, restriccionesIzquierda, restriccionesSigno, restriccionesDerecha){
+    i<-0
+    while (i<=n){
+      if (i==0)
+          
+        
+    }
+    x<-evaluarFuncion(y, x)
+    
+}
+
+
+
+https://es.slideshare.net/jaimemedrano771/programacin-lineal-entera-y-binaria
