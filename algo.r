81adab18fb797af9def6d656f1bceb2672451b26
JhonnyParraB <41595878+JhonnyParraB@users.noreply.github.com>
2018-08-21 18:08:46 -0500

    Update algo.r

--- algo.r
+++ algo.r
@@ -5,20 +5,17 @@
 #Arreglo con la parte izquierda de las restricciones
 #Arreglo con los signos de las restricciones
 #Arreglo con la parte derecha de las restricciones
-#Por defecto z toma valores positivos
 
 
 #8X1+7X2+6X3+5X4+5X5 
 #-6X1-3X2+2X3-4X4-X5+3 <= 0
 #-4X1-5X2-4X3-3X4+3X5+7 <= 0
-#ProgramacionLinealBinaria (expression(8*X1+7*X2+6*X3+5*X4+5*X5), 5, c(expression(-6*X1-3*X2+2*X3-4*X4-X5+3), expression(-4*X1-5*X2-4*X3-3*X4+3*X5+7)), c("<=","<="), c(0,0), "min")
-#ProgramacionLinealBinaria (expression(100*X1+155*X2+50*X3+112*X4+70*X5+80X6+60X7+118X8+110X9+55X10), 10, c(expression(100X1+155X2+50X3+112X4+70X5+80X6+60X7+118X8+110X9+55X10-700)), c("<="), c(0), "max")
-ProgramacionLinealBinaria<-function(z, n, restriccionesIzquierda, restriccionesSigno, restriccionesDerecha, tipoOptimizacion){
+#ProgramacionLinealBinaria (expression(8*X1+7*X2+6*X3+5*X4+5*X5), 5, c(expression(-6*X1-3*X2+2*X3-4*X4-X5+3), expression(-4*X1-5*X2-4*X3-3*X4+3*X5+7)), c("<=","<="), c(0,0))
+#ProgramacionLinealBinaria (expression(100*X1+155*X2+50*X3+112*X4+70*X5+80*X6+60*X7+118*X8+110*X9+55*X10), 10, c(expression(100*X1+155*X2+50*X3+112*X4+70*X5+80*X6+60*X7+118*X8+110*X9+55*X10-700)), c("<="), c(0))
+#ProgramacionLinealBinaria (expression(3*X1+2*X2+5*X3+2*X4+3*X5), 5, c(expression(-X1-X2+X3+2*X4-X5-1),expression(-7*X1+3*X3-4*X4-3*X5+2), expression(11*X1-6*X2-3*X4-3*X5+1)), c("<=", "<=", "<="), c(0, 0, 0))
+ProgramacionLinealBinaria<-function(z, n, restriccionesIzquierda, restriccionesSigno, restriccionesDerecha){
   
-  if (tipoOptimizacion=="max"){
-    z=parse(text=paste("-(",z,")"))
-  }
-  
+
   menorZ=0
   infactibilidadMenor=10000000000
   iMenor=0
@@ -37,23 +34,28 @@ ProgramacionLinealBinaria<-function(z, n, restriccionesIzquierda, restriccionesS
   while(numeroSolucion<=solucionesMaximas){
     if (numeroSolucion==1)
       inicio=0
-    else{
+    else
       inicio=1
-    }
     for (i in inicio:n){
+      
       infactibilidad=0
-        for (m in 1:n){
-          if (!(m %in% iMenores))
-            assign(paste("X", m, sep = ""), 0)
-          if (m==i)
-            assign(paste("X", m, sep = ""), 1)
-        }
+      
+      for (m in 1:n){
+        if (!(m %in% iMenores))
+          assign(paste("X", m, sep = ""), 0)
+        if (m==i)
+          assign(paste("X", m, sep = ""), 1)
+      }
       
       
       numeroSolucion=numeroSolucion+1
       for (j in 1:length(restriccionesIzquierda)){
         valorIzquierda=eval(restriccionesIzquierda[j])
-        cat(i,"   ", j, "   ",valorIzquierda, "|||", X1, X2, X3, X4, X5, "\n")
+        cat(i,"   ", j, "   ",valorIzquierda, "|||     ")
+        for (m in 1:n){
+          cat(get(paste("X", m, sep = "")), " ")
+        }
+        cat ("\n")
         if (!eval (  parse(text=paste(valorIzquierda, restriccionesSigno[j],restriccionesDerecha[j]))  )){
           infactibilidad=infactibilidad+abs(valorIzquierda-restriccionesDerecha[j])
         }
