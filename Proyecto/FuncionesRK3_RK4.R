155aec578b4b4ca39f831ca81ddbed701e113430
Laura Donado <ldonado11@gmail.com>
2018-10-25 23:41:47 -0500

    Update FuncionesRK3_RK4.R

--- Proyecto/FuncionesRK3_RK4.R
+++ Proyecto/FuncionesRK3_RK4.R
@@ -1,29 +1,30 @@
 #Elaborado por: Laura Donado y Jhonny Parra
 #2018
-#Preguntas:
-# Limitar h por el número de particiones o por el tamaño del salto
-# Cómo calcular el error: cómo encontrar la solución de forma analitica
 
+list.of.packages <- c("phaseR")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
 
-install.packages("phaseR")
 library(phaseR)
-
 
 f<-function(fcn,x,y){
   return(eval(fcn))
 }
 
+# Solo para prueba con dy=x+y, y(0)=1
+obtenerErrorAbsoluto<-function(x,y){
+  solucion=exp(x)*((-x*exp(-x))-exp(-x)+2)
+  return(abs(y-solucion))
+}
 
-graficarCampoPendiente<-function(xinicial, xfinal, yinicial, yfinal, fcn, numpendientes, metodo){
-  cat ("xinicial: ", xinicial, "xfinal:", xfinal, "\n")
-  cat ("yinicial: ", yinicial, "yfinal:", yfinal, "\n")
+graficarCampoPendiente<-function(x0, xn, y0, yn, fcn, numpendientes, metodo){
   apma1 <- function(t, y, parameters){
     a <- parameters[1] 
     dy <- a*(f(fcn, t, y))
     list(dy)
   } 
-  apma1.flowField <- flowField(apma1, x = c(xinicial, xfinal), 
-                               y   = c(yinicial, yfinal), parameters = c(1), 
+  apma1.flowField <- flowField(apma1, x = c(x0, xn), 
+                               y   = c(y0, yn), parameters = c(1), 
                                points = numpendientes, system = "one.dim", 
                                add = FALSE, xlab = "x", ylab = "y", 
                                main = metodo)
@@ -37,45 +38,43 @@ graficarSolucionNumerica<-function (x, y){
   }
 }
 
-rk4<-function(fcn, ti, tf, y0, h, graficar=TRUE, numpendientes=10){
-  
+rk4<-function(dy, ti, tf, y0, h, graficar=TRUE, numpendientes=10){
   t<-seq(ti, tf, h)
   y<-c(y0)
+  cat("x    |y         |k1        |k2        |k3        |k4       |error absoluto\n")
   for(i in 2:length(t)){
-    k1=h*f(fcn, t[i-1], y[i-1])
-    k2=h*f(fcn, t[i-1]+h/2, y[i-1]+h*k1*(0.5))
-    k3=h*f(fcn, t[i-1]+h/2, y[i-1]+h*k2*(0.5))
-    k4=h*f(fcn, t[i-1]+h, y[i-1]+h*k3)
+    k1=h*f(dy, t[i-1], y[i-1])
+    k2=h*f(dy, t[i-1]+h/2, y[i-1]+k1*(0.5))
+    k3=h*f(dy, t[i-1]+h/2, y[i-1]+k2*(0.5))
+    k4=h*f(dy, t[i-1]+h, y[i-1]+k3)
     y<-c(y, y[i-1]+1/6*(k1+2*k2+2*k3+k4))
+    cat(t[i-1]," | ", y[i-1]," | ",k1," | ",k2," | ",k3," | ",k4," | ",obtenerErrorAbsoluto(t[i-1],y[i-1]),"\n")
   }
   if (graficar){
-    graficarCampoPendiente(min(t), max(t), min(y), max(y), fcn, numpendientes, "RK4")
+    graficarCampoPendiente(min(t), max(t), min(y), max(y), dy, numpendientes, "RK4")
     graficarSolucionNumerica(t, y)
   }
-  rta<-list(y=y, t=t)
+  rta<-list(w=y, t=t)
 }
 
-
-rk3<-function(fcn, ti, tf, y0, h, graficar=TRUE, numpendientes=10){
+rk3<-function(dy, ti, tf, y0, h, graficar=TRUE, numpendientes=10){
   t<-seq(ti, tf, h)
   y<-c(y0)
+  cat("x    |y         |k1         |k2        |k3       |error absoluto\n")
   for(i in 2:length(t)){
-    k1=h*f(fcn, t[i-1], y[i-1])
-    k2=h*f(fcn, t[i-1]+h/2, y[i-1]+h*k1*(0.5))
-    k3=h*f(fcn, t[i-1]+h, y[i-1]-h*k1+2*k2*h)
+    k1=h*f(dy, t[i-1], y[i-1])
+    k2=h*f(dy, t[i-1]+h/2, y[i-1]+k1*(0.5))
+    k3=h*f(dy, t[i-1]+h, y[i-1]-k1+2*k2)
     y<-c(y, y[i-1]+1/6*(k1+4*k2+k3))
+    cat(t[i-1]," | ", y[i-1]," | ",k1," | ",k2," | ",k3," | ",obtenerErrorAbsoluto(t[i-1],y[i-1]),"\n")
   }
   if (graficar){
-    graficarCampoPendiente(min(t), max(t), min(y), max(y), fcn, numpendientes, "RK3")
+    graficarCampoPendiente(min(t), max(t), min(y), max(y), dy, numpendientes, "RK3")
     graficarSolucionNumerica(t, y)
   }
-  rta<-list(y=y, t=t)
+  rta<-list(w=y, t=t)
 }
 
+r<-rk4(expression(x+y), 0, 2, 1, 0.1)
 
-r<-rk4(expression(x+y), 0, 3, 1, 0.1)
-r$y
-r$t
 r2<-rk3(expression(x+y), 0, 2, 1, 0.1)
-r2$y
-r2$t
